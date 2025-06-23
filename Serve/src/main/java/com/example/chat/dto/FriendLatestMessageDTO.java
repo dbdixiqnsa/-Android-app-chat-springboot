5934183ce08eed331bceb95c1ddbe0a3b798950d
Code <1652855974@qq.com>
2025-06-23 23:48:44 +0800

    Delete Serve directory

--- Serve/src/main/java/com/example/chat/dto/FriendLatestMessageDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.chat.dto;
-
-import lombok.Data;
-
-@Data
-public class FriendLatestMessageDTO {
-    private UserDTO friend;
-    private MessageDTO latestMessage;
-}
--- Serve/src/main/java/com/example/chat/dto/SecurityAnswerDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.chat.dto;
-
-import lombok.Data;
-
-@Data
-public class SecurityAnswerDTO {
-    private String question;
-    private String answer;
-}
